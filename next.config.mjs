49f0f6e84048f0331c2e50a002d77249f2ce43d9
NelcidoDiaz <60740045+nelrafa103@users.noreply.github.com>
2024-03-20 23:01:52 -0400

    Arreglos

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   target: 'experimental-serverless-trace',
+   output: 'export'
+};
 
 export default nextConfig;
